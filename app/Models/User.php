f60a012445e28dd05a16ad5efcdd90710cd896f7
cucasa <josejarvinvalencia50@gmail.com>
2025-05-11 20:39:17 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,53 +49,44 @@ class User extends Authenticatable
         ];
     }
 
-    // ✅ Relaciones personalizadas
 
 
-    // Un usuario tiene un rol
     public function role()
     {
         return $this->belongsTo(Role::class);
     }
 
-    // Un usuario puede tener muchos artículos
     public function articles()
     {
         return $this->hasMany(Article::class);
     }
 
-    // Un usuario puede hacer muchos comentarios
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
-    // Un usuario puede hacer muchas valoraciones
     public function ratings()
     {
         return $this->hasMany(Rating::class);
     }
 
-    // Un usuario puede crear muchos reportes
     public function reports()
     {
         return $this->hasMany(Report::class);
     }
 
-    // Un usuario puede crear muchos hilos
     public function threads()
     {
         return $this->hasMany(Thread::class);
     }
 
-    // Un usuario puede hacer muchos posts
     public function posts()
     {
         return $this->hasMany(Post::class);
     }
 
 
-    // Relación: Un usuario puede crear muchos foros
     public function forums()
     {
         return $this->hasMany(Forum::class);
