d399f52aabeb31ecbeedf6d487a8c64a9810d1e6
cucasa <josejarvinvalencia50@gmail.com>
2025-05-11 20:35:16 -0500

    Update Rating.php

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,13 +8,11 @@ class Rating extends Model
 {
     protected $fillable = ['value', 'user_id', 'article_id'];
 
-    // Una valoración pertenece a un usuario
     public function user()
     {
         return $this->belongsTo(User::class);
     }
  
-    // Una valoración pertenece a un artículo
     public function article()
     {
         return $this->belongsTo(Article::class);
