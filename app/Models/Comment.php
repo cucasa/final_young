644c2932eac7b549d6bf1fc258ac9f758e96ac6b
cucasa <josejarvinvalencia50@gmail.com>
2025-05-11 20:32:33 -0500

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,21 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['body', 'user_id', 'article_id'];
+    protected $fillable = ['user_id', 'article_id'];
  
-    // Un comentario pertenece a un usuario
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Un comentario pertenece a un artículo
     public function article()
     {
         return $this->belongsTo(Article::class);
     }
 
-    // Un comentario puede ser reportado
     public function reports()
     {
         return $this->morphMany(Report::class, 'reported');
