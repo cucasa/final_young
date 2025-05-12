4eb09d6482c19765935821d3407a6cd89191d97c
cucasa <josejarvinvalencia50@gmail.com>
2025-05-11 20:31:30 -0500

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,25 +8,21 @@ class Category extends Model
 {
     protected $fillable = ['name', 'description'];
 
-    // Una categoría puede tener muchos artículos
     public function articles()
     {
         return $this->hasMany(Article::class);
     }
 
-    // Una categoría puede tener muchos hilos
     public function threads()
     {
         return $this->hasMany(Thread::class);
     }
 
-    // Una categoría puede tener muchos posts
     public function posts()
     {
         return $this->hasMany(Post::class);
     }
 
-    // Una categoría puede tener muchos foros
     public function forums()
     {
         return $this->hasMany(Forum::class);
