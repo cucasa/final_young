b71c313f1887d644af14d260a97267e45a2f8c3c
cucasa <josejarvinvalencia50@gmail.com>
2025-05-11 20:35:48 -0500

    Update Report.php

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,13 +8,11 @@ class Report extends Model
 {
     protected $fillable = ['reason', 'user_id'];
 
-    // Un reporte pertenece a un usuario (quien lo hizo)
     public function user()
     {
         return $this->belongsTo(User::class);
     }
  
-    // Un reporte puede pertenecer a muchos tipos de contenido
     public function reported()
     {
         return $this->morphTo();
