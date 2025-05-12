97cb585edefacad7876a5d5a9d13ca087f248955
cucasa <josejarvinvalencia50@gmail.com>
2025-05-11 20:30:24 -0500

    Update Article.php

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,31 +8,26 @@ class Article extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id'];
 
-    // Un artículo pertenece a un usuario
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Un artículo pertenece a una categoría
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
-    // Un artículo puede tener muchos comentarios
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
-    // Un artículo puede tener muchas valoraciones
     public function ratings()
     {
         return $this->hasMany(Rating::class);
     }
 
-    // Un artículo puede ser reportado (relación polimórfica)
     public function reports()
     {
         return $this->morphMany(Report::class, 'reported');
