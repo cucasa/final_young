6dbebff5fa72afefd186797c430b0720b658ac78
cucasa <josejarvinvalencia50@gmail.com>
2025-05-11 20:37:17 -0500

    Update Thread.php

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -8,25 +8,21 @@ class Thread extends Model
 {
     protected $fillable = ['title', 'user_id', 'forum_id', 'category_id'];
 
-    // Un hilo pertenece a un usuario
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Un hilo pertenece a un foro
     public function forum()
     { 
         return $this->belongsTo(Forum::class);
     }
 
-    // Un hilo pertenece a una categoría
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
-    // Un hilo puede tener muchos posts
     public function posts()
     {
         return $this->hasMany(Post::class);
