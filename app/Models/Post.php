6afbcc8fcbbd53f502bd71c32641cd575f3cd84a
cucasa <josejarvinvalencia50@gmail.com>
2025-05-11 20:34:22 -0500

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,25 +8,21 @@ class Post extends Model
 {
     protected $fillable = ['content', 'user_id', 'thread_id', 'category_id'];
 
-    // Un post pertenece a un usuario
     public function user()
     { 
         return $this->belongsTo(User::class);
     }
 
-    // Un post pertenece a un hilo
     public function thread()
     {
         return $this->belongsTo(Thread::class);
     }
 
-    // Un post pertenece a una categoría
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
-    // Un post puede ser reportado
     public function reports()
     {
         return $this->morphMany(Report::class, 'reported');
